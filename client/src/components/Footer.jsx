021daa9f1ba91ffb3c898c4e559050fcf2522821
oguz <oguzkarademir26@gmail.com>
2021-02-12 13:45:10 +0100

    adding social media links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,10 +8,10 @@ export const Footer = () => {
     return (
         <Container fluid>
             <Row className="p-1 navbar-fixed-bottom">
-                <Col className="text-center p-1"><a href=""><FaGithub style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
-                <Col className="text-center p-1"><a href=""><FaLinkedin style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
-                <Col className="text-center p-1"><a href=""><FaTwitter style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
-                <Col className="text-center p-1"><a href=""><FaInstagram style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
+                <Col className="text-center p-1"><a href="https://github.com/oguzkarademir" target="_blank"><FaGithub style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
+                <Col className="text-center p-1"><a href="https://www.linkedin.com/in/oguz-karademir-254493187/" target="_blank"><FaLinkedin style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
+                <Col className="text-center p-1"><a href="https://twitter.com/oguzkdemir" target="_blank"><FaTwitter style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
+                <Col className="text-center p-1"><a href="https://www.instagram.com/ogzkarademir/" target="_blank"><FaInstagram style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
                 <Col className="text-center p-1"><a href="/login" to="/login"><RiAdminFill style={{color:"#b60404", fontSize:"40px"}}/></a></Col>
             </Row>
         </Container>
